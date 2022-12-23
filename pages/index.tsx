c6ce54a3a96447c75f5b035ad0c00ecae35f1eb0
Elizabeth <elihylui@gmail.com>
2022-12-23 21:04:39 +0100

    add broadcastMessage function

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,38 @@ import { useState } from "react";
 import { osmosis } from '../codegen/';
 import md5 from "md5";
 
+import { StargateClient, IndexedTx } from "@cosmjs/stargate";
+import { Tx } from "cosmjs-types/cosmos/tx/v1beta1/tx"
+import { MsgSend } from "cosmjs-types/cosmos/bank/v1beta1/tx"
+
 export default function Home() {
+      const broadcastMessage = async(): Promise<void> => {
+        const rpc = "rpc.sentry-01.theta-testnet.polypore.xyz:26657";
+        const client = await StargateClient.connect(rpc);
+        const faucetTx: IndexedTx = (await client.getTx(
+            "540484BDD342702F196F84C2FD42D63FA77F74B26A8D7383FAA5AB46E4114A9B",
+        ))!
+        console.log("Faucet Tx:", faucetTx)
+
+        const decodedTx: Tx = Tx.decode(faucetTx.tx)
+        console.log("DecodedTx:", decodedTx)
+        console.log("Decoded messages:", decodedTx.body!.messages)
+
+        const message = await client.broadcastTx(decodedTx.body!.messages[0].value);
+        console.log(message);
+
+        // const sendMessage: MsgSend = MsgSend.decode(decodedTx.body!.messages[0].value)
+        // console.log("Sent message:", sendMessage)
+
+
+        // console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
+
+        // console.log(
+        //     "Alice balances:",
+        //     await client.getAllBalances("cosmos15kqsnekhzdquj2vfmx3p9agd5rpapktvkknfrx"), 
+        // )
+    }
+
     const [height, setHeight] = useState(0);
     const [hash, setHash] = useState("");
     const [numPools, setNumPools] = useState("numPools are not fetched");
